c6613d5f41302569047f08d45bb6e95dbb7ad46f
DaneelSergei4 <124453004+DaneelSergei4@users.noreply.github.com>
2024-02-09 07:28:03 +0300

    Update 2024_01_inputs_5.py

--- 2024_01_inputs_5.py
+++ 2024_01_inputs_5.py
@@ -2,12 +2,10 @@
 #qpy:console
 
 for row in range (0, 64):
-    rowStr = "%-5i" % (row*31);
+    rowStr = "\n%-6i" % (row*31);
     for col in range(0, 31):
         ch = chr(col + row*31);
-        if (ch.isprintable()):
-            rowStr = rowStr + "%-3s" % ch;
-        else:
-            rowStr = rowStr + "%-3s" % " ";
+        if (not ch.isprintable()):
+            ch = " ";
+        rowStr = rowStr + "%-3s" % ch;
     print(rowStr);
-
